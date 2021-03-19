73d579c824dec21b1c04c9de837da50e4d994c96
HP <bilimus@gmail.com>
2021-03-19 20:57:19 +0500

    ddf

--- src/BuyMeCoffee.java
+++ src/BuyMeCoffee.java
@@ -4,20 +4,20 @@
 public class BuyMeCoffee {
     public static void main(String[] args) {
         String str = "https://www.buymeacoffee.com/SergeiD";
-        String coded = "";
+        StringBuilder coded = new StringBuilder();
         char[] ch = str.toCharArray();
         for (char c : ch){
             c += 3;
-            coded += c;
-            //System.out.print(c);
+            coded.append(c);
         }
+
         System.out.println(coded);
-        String uncoded = "";
-        char[] chen = coded.toCharArray();
+
+        StringBuilder uncoded = new StringBuilder();
+        char[] chen = coded.toString().toCharArray();
         for (char c1 : chen){
             c1 -= 3;
-            uncoded += c1;
-            //System.out.print(c);
+            uncoded.append(c1);
         }
         System.out.println(uncoded);
 
